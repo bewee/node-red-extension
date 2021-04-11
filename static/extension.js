ffc4d736d9b36935b05eaa0833f116d272e5c02d
Benedikt Weber <benedikt.web23@gmail.com>
2021-04-11 02:18:01 +0200

    Fix style issues

--- static/extension.js
+++ static/extension.js
@@ -8,13 +8,40 @@
       window.API.getAddonConfig('node-red-extension').then((config) => {
         this.port = config.port || 1880;
         this.nodeRedUrl = `http://${window.location.hostname}:${this.port}`;
-        this.setupView();
-        document.querySelector('#menu-button').style.zIndex = '500';
+        this.addMenuObserver();
+        this.applyViewVisibility();
       });
     }
 
-    setupView() {
-      this.view.innerHTML = `<iframe src = ${this.nodeRedUrl}></iframe>`;
+    addMenuObserver() {
+      const mutationObserver = new MutationObserver(
+        (mutationsList, _observer) => {
+          const relevantMutations = mutationsList.filter((mutation) => {
+            return mutation.attributeName === 'class';
+          });
+          if (!relevantMutations || !relevantMutations[0]) {
+            return;
+          }
+          this.applyViewVisibility();
+        }
+      );
+      mutationObserver.observe(
+        document.getElementById('extension-node-red-extension-view'),
+        {attributes: true},
+      );
+    }
+
+    applyViewVisibility() {
+      const view = document.getElementById(
+        'extension-node-red-extension-view',
+      );
+      if (view.className.split(' ').includes('selected')) {
+        document.querySelector('#menu-button').style.zIndex = '500';
+        this.view.innerHTML = `<iframe src = ${this.nodeRedUrl}></iframe>`;
+      } else {
+        document.querySelector('#menu-button').style.zIndex = null;
+        this.view.innerHTML = '';
+      }
     }
 
   }
